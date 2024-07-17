36d0c4d7c1dc72910f11666d4195b32fc0f43ded
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2024-07-18 00:07:47 +0200

    add

--- src/js/task.js
+++ src/js/task.js
@@ -20,12 +20,16 @@ const expenses = {
   },
   "2023-04": {},
 };
-
+// function getFirstDay calculates the first Sunday of the given  month and year
 function getFirstDay(year, month) {
+  // create a date object for the first day of the month
   const date = new Date(year, month - 1, 1);
+  // loop runs until the day of the week is 0 (Sunday)
   while (date.getDay() !== 0) {
+    //add one day to date
     date.setDate(date.getDate() + 1);
   }
+  // return
   return date.getDate();
 }
 
@@ -35,7 +39,7 @@ function getAllExpenses(daysData, firstSunday) {
     const categories = daysData[day];
     if (parseInt(day, 10) <= firstSunday) {
       for (const category in categories) {
-        allExpenses = allExpenses.concat(categories[category]);
+        allExpenses.push(...categories[category]);
       }
     }
   }
@@ -43,17 +47,17 @@ function getAllExpenses(daysData, firstSunday) {
   return allExpenses;
 }
 
-function getMedian(expensesCount, allExpenses) {
-  let median = null;
-  if (expensesCount > 0) {
-    const sum = allExpenses.reduce(
-      (total, currentValue) => total + currentValue,
-      0
-    );
-    median = Math.round(sum / expensesCount);
-  }
+function getMedian(expenses) {
+  const count = expenses.length;
+  if (count === 0) return null;
 
-  return median;
+  const middle = Math.floor(count / 2);
+
+  if (count % 2 === 0) {
+    return (expenses[middle - 1] + expenses[middle]) / 2;
+  } else {
+    return expenses[middle];
+  }
 }
 
 function getMonthExpenses(yearMonth, expensesData) {
@@ -62,19 +66,35 @@ function getMonthExpenses(yearMonth, expensesData) {
   const daysData = expensesData[yearMonth];
   const allExpenses = getAllExpenses(daysData, firstSunday);
 
-  const { length } = allExpenses;
-  const median = getMedian(length, allExpenses);
-
-  return median;
+  return getMedian(allExpenses);
 }
 
-function solution(expensesData) {
-  const result = {};
+function getMedianOfMedian(expenses) {
+  const medians = [];
 
-  for (const yearMonth in expensesData) {
-    result[yearMonth] = getMonthExpenses(yearMonth, expensesData);
+  for (const yearMonth in expenses) {
+    const median = getMonthExpenses(yearMonth, expenses);
+    if (median !== null) {
+      medians.push(median);
+    }
   }
-
+  return medians;
+}
+function medianOfAllMonth(medians) {
+  const lengthOfMedian = medians.length;
+  const middle = Math.floor(lengthOfMedian / 2);
+  if (lengthOfMedian === 0) return null;
+  if (lengthOfMedian % 2 === 0) {
+    return (medians[middle - 1] + medians[middle]) / 2;
+  } else {
+    return medians[middle];
+  }
+}
+// function take object with expenses
+function solution(expenses) {
+  let result = null;
+  const medians = getMedianOfMedian(expenses);
+  result = medianOfAllMonth(medians);
   return result;
 }
 
